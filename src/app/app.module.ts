779763245ea0c747f7126065c4214871548e1a5c
Sven Büttner <s.buettner@friends-of-bytes.com>
2023-08-26 20:40:31 +0200

    add Services and Components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { MessageLogDisplayComponent } from './components/message-log-display/message-log-display.component';
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [						
+    AppComponent,
+      FileUploadComponent,
+      MessageLogDisplayComponent,
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
