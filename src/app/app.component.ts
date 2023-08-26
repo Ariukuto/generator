c1d972b24d6e1192dec75caa93d17849510fdce3
Sven Büttner <s.buettner@friends-of-bytes.com>
2023-08-26 20:41:23 +0200

    subscribe on settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { SettingsService } from './services/settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'generator';
+export class AppComponent implements OnInit
+{
+  protected readonly settingsService = inject(SettingsService);
+ 
+
+  protected settings: any;
+
+  ngOnInit(): void {
+    this.settingsService.settings.subscribe(v => this.settings = v);
+  }
 }
